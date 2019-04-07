be2f49c9543d23b5f164eac89e4612676b8acebb
Ashish Kumar yadav <ashishkumar.yadav@isentia.com>
2019-04-07 22:55:11 +0800

    Remvoed js files
    Remvoed js files

--- server/app/dataLayer/tableSchema.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Tables;
-(function (Tables) {
-    Tables["Records"] = "records";
-})(Tables = exports.Tables || (exports.Tables = {}));
